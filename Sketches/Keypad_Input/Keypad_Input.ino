6ab25e0e3e420a07e82a0e0c1380582bc039c37a
Andre D'Souza <adsouz03@uoguelph.ca>
2022-03-05 00:35:07 -0500

    Sketch to add the User Input functionality using a 1x4 Keypad

---
+++ Sketches/Keypad_Input/Keypad_Input.ino
@@ -0,0 +1,55 @@
+#include "Adafruit_Keypad.h"
+
+const byte ROWS = 1; //Rows
+const byte COLS = 4; //Columns
+//define the symbols on the buttons of the keypads
+char keys[ROWS][COLS] = {'1','2','3','4'};
+byte rowPins[ROWS] = {A1};
+byte colPins[COLS] = {A2,A3,A4,A5};
+Adafruit_Keypad customKeypad = Adafruit_Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS);
+
+void setup() {
+  Serial.begin(9600);
+  customKeypad.begin();
+}
+
+void loop() {
+
+  int routingTable[4];
+  keypadInput(routingTable);
+
+  Serial.println("Route is: ");
+  for (int i=0; i<5; i++) {
+    Serial.println(routingTable[i]);
+  }
+  while(1){}
+}
+
+void keypadInput(int route[])
+{
+  int count = 0;
+  int tmp = 0;
+  do {
+    customKeypad.tick();
+    while(customKeypad.available()){
+      keypadEvent e = customKeypad.read();
+
+      if (e.bit.EVENT == KEY_JUST_PRESSED) {
+        Serial.print((char)e.bit.KEY);
+        Serial.println(" pressed");
+        tmp = (char)e.bit.KEY - '0';
+        route[count] = tmp;
+        count++;
+      }
+      if (count == 4) {
+        route[4] = 0;
+        count = 5;
+      }
+    }
+
+  } while (count < 5);
+
+  return route;
+}
+
+
